f26310780b765bc2fc800f70e7500e2a2916ed1d
Udofalcon <udofalconmillstone@gmail.com>
2023-05-27 19:39:31 -0400

    A little bit of extra logging.

--- Twitch/src/index.ts
+++ Twitch/src/index.ts
@@ -16,7 +16,7 @@ const TWITCH_CLIENT_SECRET: string = process.env.TWITCH_CLIENT_SECRET as string;
 const TWITCH_PORT: number = parseInt(process.env.TWITCH_PORT as string, 10);
 const TWITCH_ACCESS_TOKEN = oauth.getAccessToken(TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET).then(
     (value: unknown) => { console.log(value); },
-    (reason: any) => { console.error(reason); }
+    (reason: any) => { console.error('oauth.getAccessToken', reason); }
 );
 
 const app = express();
